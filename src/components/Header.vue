5b3e63917e7697024ef59178faf5ae0ba224f9e9
jurasora1010 <ikumi.hamana@rakus-partners.co.jp>
2021-11-19 14:35:12 +0900

    ヘッダーとフッターを更新

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>ヘッダーです</div>
+  <header>
+    <div class="container">
+      <div class="header">
+        <div class="header-left">
+          <a href="top.html">
+            <img class="logo" src="img/header_logo.png" />
+          </a>
+        </div>
+
+        <div class="header-right">
+          <a href="item_list.html">商品一覧</a>
+          <a href="register_admin.html">会員登録</a>
+          <a href="#"><i class="fas fa-shopping-cart"></i>カート</a>
+          <a href="login.html" class="login">
+            <i class="fas fa-sign-in-alt"></i>ログイン
+          </a>
+
+          <a href="order_history.html">注文履歴</a>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
