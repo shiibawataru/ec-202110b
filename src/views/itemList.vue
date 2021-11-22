90454dcd67251f0fb07fe9f0e7db48e06645981c
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2021-11-22 13:38:12 +0900

    CSSの導入

--- src/views/itemList.vue
+++ src/views/itemList.vue
@@ -3,131 +3,17 @@
   <div class="item-wrapper">
     <div class="container">
       <div class="items">
-        <div class="item">
-          <div class="item-icon">
-            <img src="/img/1.jpg" />
+        <ul v-for="item of itemList" v-bind:key="item.id">
+          <div class="item">
+            <div class="item-icon">
+              <img v-bind:src="item.imagePath" />
+            </div>
+            <a href="item_detail.html"> {{ item.name }}</a
+            ><br />
+            <span class="price">Ｍ</span>{{ item.priceM }}<br />
+            <span class="price">Ｌ</span>{{ item.priceL }}<br />
           </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/2.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/3.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/4.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/5.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/6.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/7.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/8.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/9.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/10.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/11.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/12.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/13.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
-
-        <div class="item">
-          <div class="item-icon">
-            <img src="img/14.jpg" />
-          </div>
-          <a href="item_detail.html">ハワイアンパラダイス</a><br />
-          <span class="price">Ｍ</span>1,380円(税抜)<br />
-          <span class="price">Ｌ</span>2,380円(税抜)<br />
-        </div>
+        </ul>
       </div>
     </div>
   </div>
@@ -141,11 +27,120 @@ import { Item } from "@/types/Item";
 export default class ItemList extends Vue {
   // 商品一覧
   private itemList: Array<Item> = [];
-
+  private imgUrl = "/img/";
+  //
   async created(): Promise<void> {
     await this["$store"].dispatch("getItemList");
     this.itemList = this["$store"].getters.getItemList;
   }
 }
 </script>
-<style></style>
+<style scoped>
+/* ========================================
+    商品一覧ページのスタイル
+   ======================================== */
+
+/* ========================================
+    search-wrapperの設定
+   ======================================== */
+
+.search-wrapper {
+  padding: 80px 0 50px 0; /*上はヘッダが来るのでその分180px分空ける*/
+  /* text-align: center; */
+  position: fixed; /* スクロールしても表示されるように位置を固定 */
+  height: 180px;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.9);
+  z-index: 1; /* 上に表示されるようにする(数が多いほど上に来る。例えば-1にすると裏側に行き見えなくなります) */
+}
+
+.search-form {
+  margin: auto; /*検索窓を中央に配置*/
+  width: 40%;
+  text-align: center;
+}
+
+.search-name-input {
+  height: 30px;
+  padding: 5px 10px;
+  font-size: 12px;
+  line-height: 1.5;
+  border-radius: 3px;
+  /* display: block; */
+  width: 30%;
+  color: #555;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  font-family: inherit;
+  margin: 0;
+  text-rendering: auto;
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  text-align: start;
+  appearance: auto;
+  -webkit-rtl-ordering: logical;
+  cursor: text;
+}
+
+.search-btn {
+  color: #fff;
+  background-color: #337ab7;
+  border-color: #2e6da4;
+  display: inline-block;
+  padding: 6px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  font-weight: normal;
+  line-height: 1.42857143;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  touch-action: manipulation;
+  cursor: pointer;
+  user-select: none;
+  background-image: none;
+  border: 1px solid transparent;
+  border-radius: 4px;
+}
+
+/* ========================================
+    item-wrapperの設定
+   ======================================== */
+
+.item-wrapper {
+  padding-top: 200px; /* 上はヘッダや検索フォームが来るのでその分空ける */
+  padding-bottom: 80px;
+  padding-left: 15%;
+  padding-right: 5%;
+  background-color: #f7f7f7;
+  text-align: center;
+}
+
+.items {
+  display: flex;
+  flex-wrap: wrap; /*表示内容が多かった時に自動的に複数行に分割される */
+}
+
+.item {
+  flex: 0 0 320px; /* paddingやborder含むitem全体の横幅を320pxにする */
+  padding: 20px;
+}
+
+.item-icon img {
+  margin: auto;
+  display: block;
+  border-radius: 30px;
+  width: 200px;
+  height: 200px;
+  padding: 0 0 15px 0;
+}
+
+/* サイズをオレンジ〇で囲む */
+.price {
+  background-color: #ff4500;
+  border-radius: 50%; /* 角丸にする設定 */
+}
+</style>
