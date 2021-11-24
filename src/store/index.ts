72c0102dc1f16890a6ca2acb7667f66ef07a1de3
koda-momo <momoko.kodama@rakus-partners.co.jp>
2021-11-24 12:03:20 +0900

    indexのstoreにログインIDを置けるように修正

--- src/store/index.ts
+++ src/store/index.ts
@@ -12,6 +12,8 @@ export default new Vuex.Store({
     items: new Array<Item>(),
     //カートの商品情報
     cartList: new Array<OrderItem>(),
+    //ログインしているユーザID
+    userId: "",
   },
   mutations: {
     /**
@@ -66,6 +68,13 @@ export default new Vuex.Store({
     deleteItem(state, index) {
       state.cartList.splice(index, 1);
     },
+
+    /**
+     * ログイン中のユーザIDを取得する.
+     */
+    loginUserId(state, userId) {
+      state.userId = userId;
+    },
   }, // end mutations
   actions: {
     /**
@@ -103,6 +112,15 @@ export default new Vuex.Store({
      */
     getCartList(state) {
       return state.cartList;
+    },
+
+    /**
+     * ユーザIDを返す.
+     * @param state ステート
+     * @returns ユーザID
+     */
+    getUserId(state) {
+      return state.userId;
     },
   },
 });
