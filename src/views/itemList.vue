4e50fef7f1877c1994f0799a91d5524dfb533a6f
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2021-11-25 16:34:46 +0900

    アイテムリストページの雪

--- src/views/itemList.vue
+++ src/views/itemList.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div class="top-wrapper">
+    <div class="snow">●</div>
+
     <!-- search form -->
     <div class="container">
       <form method="post" class="search-form">
@@ -236,5 +238,28 @@ export default class ItemList extends Vue {
 
 .btn {
   margin-right: 30px;
+}
+.top-wrapper {
+  background-color: #dc143c;
+}
+.container {
+  background-color: lightgray;
+}
+.snow {
+  color: snow; /*雪の色*/
+  font-size: 25px; /*雪の大きさ*/
+  position: fixed;
+  top: -5%; /*初期位置*/
+  text-shadow: 5vw -100px 2px, 10vw -400px 3px, 20vw -500px 4px, 30vw -580px 1px,
+    39vw -250px 2px, 42vw -340px 5px, 56vw -150px 2px, 63vw -180px 0,
+    78vw -220px 4px, 86vw -320px 9px, 94vw -170px 7px;
+  animation: anim 5s linear infinite;
+}
+
+@keyframes anim {
+  100% {
+    color: transparent;
+    top: 150%;
+  }
 }
 </style>
